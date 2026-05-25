eedaaedd66d6641c10d0c60f03ef4e35fdfb4a44
StefanThomas7 <stefan.thomas@nih.gov>
2026-05-25 09:10:32 -0700

    Lots of changes to project organization. Aded some CSS for beautification.

--- module_1/personal_website/app/__init__.py
+++ module_1/personal_website/app/__init__.py
@@ -1,1 +1,33 @@
-#__init__.py initializes yourapplication creating a  flask instance
+#__init__.py initializes your application creating a  flask instance
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__) # Flask constructor
+
+# A decoratore used ot tell the applicateion
+# the URL is assocaited function
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    # You can render a new html template here for biography
+    return "The about page"
+    # return render_template('stefanThomasBiography.html')   
+
+@app.route('/contactInfo')
+def contactInfo():
+    return "Use this page to show contact info html"
+    # return render_template('contactInfo.html')
+
+
+
+
+
+
+# Where are we running?
+if __name__ == '__main__':
+    # RUn the application
+    app.run(host = '0.0.0.0', port = 8080)
