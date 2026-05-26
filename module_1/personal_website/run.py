40536514f341e2b74e453d737e3fe1499a0ca7df
StefanThomas7 <stefan.thomas@nih.gov>
2026-05-25 17:12:37 -0700

    Very close to MVP. Nav bar working. Bio complete. Spacing is whatever, but i'll work on it more later.
    Need to link projects according to assignment criteria

--- module_1/personal_website/run.py
+++ module_1/personal_website/run.py
@@ -7,4 +7,4 @@ app = create_app()
 # Where are we running?
 if __name__ == '__main__':
     # Run the application
-    app.run(host = '0.0.0.0', port = 8080)
+    app.run(host = '0.0.0.0', port = 8080, debug=True)
--- module_1/personal_website/webApp/pages/pages.py
+++ module_1/personal_website/webApp/pages/pages.py
@@ -1,8 +1,7 @@
 # Creating blueprint for pages so that all pages share the same template
 from flask import Blueprint, render_template
 
-bp = Blueprint("pages", __name__,
-                template_folder='templates')
+bp = Blueprint("pages", __name__, template_folder='templates')
 
 
 # A decorator used to tell the application
@@ -11,13 +10,10 @@ bp = Blueprint("pages", __name__,
 def home():
     return render_template('home.html')
 
-@bp.route('/about')
-def about():
-    # You can render a new html template here for biography
-    return "The about page"
-    # return render_template('stefanThomasBiography.html')   
-
 @bp.route('/contactInfo')
 def contactInfo():
-    return "Use this page to show contact info html"
-    # return render_template('contactInfo.html')
+    return render_template('contactInfo.html')
+
+@bp.route('/projects')
+def projects():
+    return render_template('projects.html')
